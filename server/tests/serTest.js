e50a86e6173bd3524a77eb9788b34e1b71b12e5b
pfnegrini <kingsta@protonmail.com>
2020-07-02 22:20:04 +0200

    e

--- server/tests/serTest.js
+++ server/tests/serTest.js
@@ -24,6 +24,6 @@ sPort.on('open',function() {
 sPort.on('data', function(data) {
   data = data.toString('utf8');
 console.log(data.trim());
-sPort.write("SCMD sdsds");
+serialPort.write("SCMD sdsds");
 
 });
