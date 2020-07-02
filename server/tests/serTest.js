402e4308f1591901cfe4a1f2d3f4e60eccbc2aba
pfnegrini <kingsta@protonmail.com>
2020-07-02 20:56:54 +0200

    added test folder

---
+++ server/tests/serTest.js
@@ -0,0 +1,24 @@
+// * @emits open
+// * @emits data
+// * @emits close
+// * @emits error
+// * @alias module:serialport
+
+const serPort = "/dev/ttyACM0"
+const serBaud = "9600"
+
+
+const com = require('serialport')
+const Readline = require('@serialport/parser-readline')
+const sPort = new com(serPort,{
+  baudRate: Number(serPort)
+});
+
+const serialPort = sPort.pipe(new Readline({ delimiter: '\r\n' }))
+
+
+sPort.on('open',function() {
+  console.log('Arduino connected on '+ serPort + ' @' + serBaud)
+})
+
+serialPort.on('data', console.log)
