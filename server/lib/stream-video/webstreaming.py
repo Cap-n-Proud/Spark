c91013ba12eb8f0bd8428d72c50889c42fd1b72f
Cap-n-Proud <kingsta@protonmail.com>
2020-07-27 13:04:18 +0200

    u

--- server/lib/stream-video/webstreaming.py
+++ server/lib/stream-video/webstreaming.py
@@ -39,8 +39,8 @@ import socket
 # IP address
 def get_IP():
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
+	s.connect(("8.8.8.8", 80))
+	return s.getsockname()[0]
 
 # Driver code
  #Function call
