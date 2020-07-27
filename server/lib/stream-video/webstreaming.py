c29e719f573b5471958ecce5a353044e62cf3c68
Cap-n-Proud <kingsta@protonmail.com>
2020-07-27 23:04:31 +0200

    t

--- server/lib/stream-video/webstreaming.py
+++ server/lib/stream-video/webstreaming.py
@@ -106,14 +106,14 @@ def drawCompass(im, videoWidth, videoHeight, heading):
 			cv2.line(im, ((i+heading)+minI, - screenMargin), ((i+heading)+minI, + screenMargin + 10), (hudColorR,hudColorG,hudColorB))
 		if i%10==0:
 			cv2.line(im, ((i+heading)+minI, - screenMargin), ((i+heading)+minI, + screenMargin + 5), (hudColorR,hudColorG,hudColorB))
-
-		if (i == map(0,0,videoWidth,minI, maxI)):
+		K = 1.2
+		if (i == K * map(0,0,360,minI, maxI)):
 			cv2.putText(im, "N", (reduce(i+heading+videoWidth//2,compassRange)+minI, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (onScreenColorR, onScreenColorG, onScreenColorB))
-		if (i == map(270,0,videoWidth,minI, maxI)):
+		if (i == K * map(270,0,360,minI, maxI)):
 			cv2.putText(im, "E", (reduce(i+heading+videoWidth//2,compassRange)+minI, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (onScreenColorR, onScreenColorG, onScreenColorB))
-		if (i == map(180,0,videoWidth,minI, maxI)):
+		if (i == K * map(180,0,360,minI, maxI)):
 			cv2.putText(im, "S", (reduce(i+heading+videoWidth//2,compassRange)+minI, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (onScreenColorR, onScreenColorG, onScreenColorB))
-		if (i == map(90,0,videoWidth,minI, maxI)):
+		if (i == K * map(90,0,360,minI, maxI)):
 			cv2.putText(im, "W", (reduce(i+heading+videoWidth//2,compassRange)+minI, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.35,  (onScreenColorR, onScreenColorG, onScreenColorB))
         # drawHeading(im, videoWidth, videoHeight, heading);
 
