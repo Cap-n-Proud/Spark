07c30f53d38ff0584f3fb02a838b1602d28f9d28
Cap-n-Proud <kingsta@protonmail.com>
2020-07-27 15:43:45 +0200

    t

--- server/lib/stream-video/webstreaming.py
+++ server/lib/stream-video/webstreaming.py
@@ -97,11 +97,15 @@ def drawCompass(im, videoWidth, videoHeight, heading):
 	minI = 0
 	maxI = 360
 	compassRange = maxI-minI
-	for i in range(0, 640):
+	for i in range(minI, maxI):
+		# if i%20==0:
+		# 	cv2.line(im, (reduce(i+heading,compassRange)+minI, - screenMargin), (reduce(i+heading,compassRange)+minI, + screenMargin + 10), (hudColorR,hudColorG,hudColorB))
+		# if i%10==0:
+		# 	cv2.line(im, (reduce(i+heading,compassRange)+minI, - screenMargin), (reduce(i+heading,compassRange)+minI, + screenMargin + 5), (hudColorR,hudColorG,hudColorB))
 		if i%20==0:
-			cv2.line(im, (reduce(i+heading,compassRange)+minI, - screenMargin), (reduce(i+heading,compassRange)+minI, + screenMargin + 10), (hudColorR,hudColorG,hudColorB))
+			cv2.line(im, ((i+heading)+minI, - screenMargin), ((i+heading)+minI, + screenMargin + 10), (hudColorR,hudColorG,hudColorB))
 		if i%10==0:
-			cv2.line(im, (reduce(i+heading,compassRange)+minI, - screenMargin), (reduce(i+heading,compassRange)+minI, + screenMargin + 5), (hudColorR,hudColorG,hudColorB))
+			cv2.line(im, ((i+heading)+minI, - screenMargin), ((i+heading)+minI, + screenMargin + 5), (hudColorR,hudColorG,hudColorB))
 	#
 		#test
         # if (i == map(0,0,360,minI, maxI))
