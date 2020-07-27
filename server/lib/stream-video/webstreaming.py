c8c5cbd9ef37b0faadda6be89bd8edd5d39801c2
Cap-n-Proud <kingsta@protonmail.com>
2020-07-27 11:45:52 +0200

    d

--- server/lib/stream-video/webstreaming.py
+++ server/lib/stream-video/webstreaming.py
@@ -71,7 +71,7 @@ def detect_motion(frameCount):
 		# cv2.putText(frame, str(yprh[1]), (80, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 255, 0), 1)
 		# cv2.putText(frame, str(yprh[0]), (120, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 255, 0), 1)
 		cv2.putText(frame, str(int(1.0/(time.time()-start_time))), (10, frame.shape[0] - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 255, 0), 1)
-
+		print(str(int(1.0/(time.time()-start_time))))
 
 		# if the total number of frames has reached a sufficient
 		# number to construct a reasonable background model, then
