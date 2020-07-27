41e085d9ca6c1fc3821056e0634a8271258e621b
Cap-n-Proud <kingsta@protonmail.com>
2020-07-27 12:44:25 +0200

    u

--- server/lib/stream-video/webstreaming.py
+++ server/lib/stream-video/webstreaming.py
@@ -25,9 +25,10 @@ yprh=[0,0,0,0]
 import pathlib
 p = pathlib.Path(__file__).parent.absolute()
 print(str(p.parent.parent) + '/config.json')
-# parse x:
-#config = json.loads(__dirname + '/config.json')
 
+# parse x:
+config = json.loads(str(p.parent.parent) + '/config.json')
+print(config['video']['FPS'])
 # standard Python
 sio = socketio.Client()
 #sio.wait()
