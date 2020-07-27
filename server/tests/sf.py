9844682495aea57e375aab4ddbc23c7b9e5f2480
Cap-n-Proud <kingsta@protonmail.com>
2020-07-27 10:18:25 +0200

    u

--- server/tests/sf.py
+++ server/tests/sf.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+sio = socketio.Client()  # !!! at this point i receive a new connection on remote server, however the code freezes on this line and the script hang on until the end of the connection
+sio = socketio.Connect('192.168.1.50', '54321')
+
 @socketio.on('yprh')
 def handle_json(*yprh):
     print('received json: ' + str(yprh))
