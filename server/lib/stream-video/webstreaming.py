a266ec6ce1cfa86d962ff40fbf4e8986b30b0dbf
Cap-n-Proud <kingsta@protonmail.com>
2020-07-27 12:57:53 +0200

    e

--- server/lib/stream-video/webstreaming.py
+++ server/lib/stream-video/webstreaming.py
@@ -177,14 +177,14 @@ sio.connect('http://192.168.1.50:54321')
 	# check to see if this is the main thread of execution
 if __name__ == '__main__':
 	# construct the argument parser and parse command line arguments
-	# ap = argparse.ArgumentParser()
+	 ap = argparse.ArgumentParser()
 	# ap.add_argument("-i", "--ip", type=str, required=True,
 	# 	help="ip address of the device")
 	# ap.add_argument("-o", "--port", type=int, required=True,
 	# 	help="ephemeral port number of the server (1024 to 65535)")
-	# ap.add_argument("-f", "--frame-count", type=int, default=32,
-	# 	help="# of frames used to construct the background model")
-	# args = vars(ap.parse_args())
+	 ap.add_argument("-f", "--frame-count", type=int, default=32,
+	 	help="# of frames used to construct the background model")
+	 args = vars(ap.parse_args())
 
 	# start a thread that will perform motion detection
 	t = threading.Thread(target=detect_motion, args=(
